b37963fa92d8c489223a723fd0da968655595392
Nityangana <nityangana86@yahoo.com>
2025-01-25 08:38:18 +0530

    Assignment 5 updated

---
+++ 05-arrays/Easy/Question13.java
@@ -0,0 +1,30 @@
+// Flipping an Image
+
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int[][] invert = new int[image.length][image.length];
+
+        // flip the image horizontally
+        for (int i=0; i<image.length; i++) {
+            int k = 0;
+            for (int j=image.length-1; j>=0; j--) {
+                invert[i][k] = image[i][j];
+                k++;
+            }
+        }
+
+        // invert the image
+        for (int[] i: invert) {
+            for (int j=0; j<i.length; j++) {
+                if (i[j]==1) {
+                    i[j]=0;
+                }
+                else {
+                    i[j]=1;
+                }
+            }
+        }
+
+        return invert;
+    }
+}
